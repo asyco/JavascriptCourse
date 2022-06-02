9e2ff16821e972d0c49898077a515b8603e24fc1
mypage <63529035+asyco@users.noreply.github.com>
2022-06-02 12:15:11 -0500

    prueba x

---
+++ figuras.js
@@ -0,0 +1,24 @@
+const ladoCuadrado = 5; //cm
+const perimetroCuadrado = 4*ladoCuadrado; //cm
+const areaCuadrado = ladoCuadrado*ladoCuadrado; // cm2
+
+console.log("El lado del cuadrado es: " + ladoCuadrado + "cm");
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+console.log("El área del cuadrado es: " + areaCuadrado + "cm2");
+
+// TRIÁNGULO
+
+const lado1Triangulo = 6; //cm
+const lado2Triangulo = 6; //cm
+const baseTriangulo = 4; //cm
+
+console.log(
+    "Los lados del triángulo miden: "
+    + lado1Triangulo
+    + "cm, "
+    + lado2Triangulo
+    + "cm, "
+    + baseTriangulo
+    + "cm"
+);
+
