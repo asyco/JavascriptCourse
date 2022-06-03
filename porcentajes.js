7bcf7b018c54c4aadba51eca609a8f8b1e21707b
mypage <63529035+asyco@users.noreply.github.com>
2022-06-02 20:16:18 -0500

    funcionalidad de cupones

--- porcentajes.js
+++ porcentajes.js
@@ -3,3 +3,53 @@ function precioConDescuento(precio, descuento){
     const precioNuevo = (precio * porcentajePrecioConDescuento) / 100;
     return precioNuevo;
 }
+
+function calcularPrecioFinal(){
+    const inputPrecio = document.getElementById("Inputprecio");
+    const precioValor = inputPrecio.value;
+
+    const inputDescuento= document.getElementById("Inputdescuento");
+    const descuentoValor = inputDescuento.value;
+
+    const precioFinal = precioConDescuento(precioValor, descuentoValor);
+
+    const resultado = document.getElementById("resultadoFinal");
+    resultado.innerText = "El precio a pagar es: $" + precioFinal;
+    
+}
+
+// Módulo para aplicar cupón
+function aplicarCupon(){
+    const inputPrecio = document.getElementById("Inputprecio");
+    const precioValor = inputPrecio.value;
+
+    const inputDescuento= document.getElementById("Inputdescuento");
+    const descuentoValor = inputDescuento.value;
+
+    const precioFinal = precioConDescuento(precioValor, descuentoValor);
+
+    // cupones
+    var cupones = ["abc123", "eef338", "xxc445", "rfr333", "ujj800"];
+    const inputCupon = document.getElementById("InputCupon");
+    const cuponValor = inputCupon.value;
+    
+    var cupon = false;
+    for (cup of cupones){
+        if (cup == cuponValor ){
+            cupon = true;
+        }
+    }
+    if (cupon){
+        var resultadoCupon = document.getElementById("resultadocupon");
+        var nuevoPrecio = precioFinal * 0.95;
+        resultadoCupon.innerText = "Sí aplica cupón del 5%, el nuevo valor a pagar es: " + nuevoPrecio;
+    }
+    else{
+        var resultadoCupon = document.getElementById("resultadocupon");
+        resultadoCupon.innerText = "NO aplica cupón";
+    }
+    
+        
+}
+
+    
